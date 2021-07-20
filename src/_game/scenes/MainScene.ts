6042008f8026cc69d7625b5d91d3177963c98384
Migue <mmoyano16@gmail.com>
2021-07-20 20:35:09 -0300

    se agregan transformers

--- src/_game/scenes/MainScene.ts
+++ src/_game/scenes/MainScene.ts
@@ -1,11 +1,57 @@
 import Phaser from "phaser";
+import ChobiPng from "../assets/img/chobi.png";
+
 
 export default class MainScene extends Phaser.Scene {
+
+    // propiedades
+    // -----------------------
+    private chobi:Phaser.GameObjects.Image;
+    private cursors:Phaser.Types.Input.Keyboard.CursorKeys;
+
+
+    // constructor
+    // -----------------------
+
     constructor() {
         super("MainScene");
     }
 
-    create() {
-        this.add.text(20,20,"que bien todo anda joia");
+    // bucle del juego
+    // -----------------------
+
+    preload() {
+        this.load.image("Chobi",ChobiPng);
     }
-}
+
+    create() {
+        this.crearChobi();
+        this.crearCursores();
+    }
+
+    update() {
+        this.controlarMovimiento();
+    }
+
+    // metodos de creacion
+    // -----------------------
+
+    crearChobi() {
+        this.chobi = this.add.image(40,40,"Chobi");
+    }
+
+    crearCursores() {
+        this.cursors = this.input.keyboard.createCursorKeys();
+    }
+
+    // metodos de control
+    // -----------------------
+
+    controlarMovimiento() {
+        if (this.cursors.up.isDown) this.chobi.y-=2;
+        if (this.cursors.down.isDown) this.chobi.y+=2;
+        if (this.cursors.left.isDown) this.chobi.x-=2;
+        if (this.cursors.right.isDown) this.chobi.x+=2;
+    }
+
+}   
